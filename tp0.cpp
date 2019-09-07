54b9d3a74d7b80de77f65897e051f16a4ee042fb
Axel <axelmpm@gmail.com>
2019-09-07 13:49:34 -0300

    Crea el make up del tp

--- tp0.cpp
+++ tp0.cpp
@@ -8,7 +8,8 @@ typedef struct matrix {
     double* array;
 } matrix_t;
 
-int error(){
+int error(){ //COMENTARIO PARA NOSOTROS: Esta funcion deberia ejecutarse siempre que haya un error y debeira hacer lo que haya que hacer ante el y luego cortar el programa
+             // si hay estructuras de datos con memoria ya almacenada primero deberan liberarse y despues cortar el programa
     return 0;
 }
 void outputFile(char fileName[]){
@@ -34,6 +35,7 @@ void outputFile(char fileName[]){
 
 int main(int argc, const char* argv[])
 {
+    //HANDELING COMANDS
     int a = 1; //COMENTARIO PARA NOSOTROS: Por alguna razon si no pongo a y pongo 1 en vez no anda
 
     if (strcmp(argv[a],"-h") == 0 || strcmp(argv[a],"--help") == 0){
@@ -46,6 +48,35 @@ int main(int argc, const char* argv[])
         outputFile(fileName);
     }
 
+
+    //MAIN PROGRAM
+
+    int dimention = readMatrixDimention(); //COMENTARIO PARA NOSOTROS: Lee el primer valor ingresado por stdin (que supuestamente es el N. Si no es un numero ejectua "error()"
+
+    matrix_t* matrix_a = create_matrix(dimention,dimention);
+    matrix_t* matrix_b = create_matrix(dimention,dimention);
+
+    fillUpMatrices(matrix_a,matrix_b); //COMENTARIO PARA NOSOTROS: Lee los siguientes valores que se ingresan por stdin hasta llegar al \n y corta. Si en algun momento hay algo fuera de lugar
+                                       //ejecuta a "error()"
+
+    matrix_t* matrix_c = matrix_multiply(matrix_a,matrix_b);
+
+    //print_matrix(fp,matrix_c);
+
+    destroy_matrix(matrix_a);
+    destroy_matrix(matrix_b);
+    destroy_matrix(matrix_c);
+
+    /*COMENTARIO PARA NOSOTROS:
+    Resumiendo, hay que hacer las funciones:
+
+    readMatrixDimention
+    create_matrix
+    fillUpMatrices
+    matrix_multiply
+    destroy_matrix
+    error
+    */
 
     return 0;
 }
