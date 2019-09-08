0af0d5363b6c705760b859bb9758ec42a0c877bd
Nacho <ignacio.boada@outlook.com>
2019-09-08 02:50:57 -0300

    destroy y create en tp0.c

---
+++ Tp0.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "Tp0.h"
+
+struct matrix {
+    size_t rows;
+    size_t cols;
+    double* array;
+};
+
+int error(){ //COMENTARIO PARA NOSOTROS: Esta funcion deberia ejecutarse siempre que haya un error y debeira hacer lo que haya que hacer ante el y luego cortar el programa
+             // si hay estructuras de datos con memoria ya almacenada primero deberan liberarse y despues cortar el programa
+    return 0;
+}
+//PRE: RECIBE UN CHAR*
+//POST: DEVUELVE LA DIMENSION O 0(CERO) SI HAY ERROR. NO SE ACEPTA DIMENSION 0.
+int readMatrixDimention(char* dimLeida){
+    int codigoErrorDimension=123;
+    int dimension = atoi(dimLeida);
+    if (dimension==0){
+        error();//emitir error
+        exit(codigoErrorDimension);
+    }//end true if condition
+    return dimension;
+};
+
+void outputFile(char fileName[]){
+    //ADAPTS FILE NAME
+    char s[100];
+    strcat(s, "./");
+    strcat(s, fileName);
+
+    //TRIES TO OPEN FILE
+    FILE *fp;
+    fp = fopen(s,"r");
+    if(fp == NULL){error();}
+
+    //OUTPUTS
+    char c;
+	do
+    {
+        c = getc(fp);
+		printf("%c",c);
+	}
+	while(c != EOF);
+}
+
+// Constructor de matrix_t
+matrix_t* create_matrix(size_t rows, size_t cols){
+    matrix_t *matriz = malloc(sizeof(matrix_t));
+    if (matriz == NULL){
+        error("MatrizNull");
+    }
+    matriz->array = malloc(sizeof(size_t) * cols * rows);
+    if (matriz->array == NULL){
+        free(matriz);
+        error("no hay memoria suficiente para crear la matriz");
+    }
+    matriz->rows = rows;
+    matriz->cols = cols;
+    return matriz;
+}
+
+
+// Destructor de matrix_t
+void destroy_matrix(matrix_t* m){
+    free(m->array);
+    free(m);
+}
+
+int main(int argc, const char* argv[])
+{
+    //HANDELING COMANDS
+    int a = 1; //COMENTARIO PARA NOSOTROS: Por alguna razon si no pongo a y pongo 1 en vez no anda
+
+    if (strcmp(argv[a],"-h") == 0 || strcmp(argv[a],"--help") == 0){
+        char fileName[] = "help";
+        outputFile(fileName);
+    }
+
+    if (strcmp(argv[a],"-V") == 0 || strcmp(argv[a],"--version") == 0){
+        char fileName[] = "version";
+        outputFile(fileName);
+    }
+
+
+    //MAIN PROGRAM
+
+    int dimention = readMatrixDimention(); //COMENTARIO PARA NOSOTROS: Lee el primer valor ingresado por stdin (que supuestamente es el N. Si no es un numero ejectua "error()"
+
+    matrix_t* matrix_a = create_matrix(dimention,dimention);
+    matrix_t* matrix_b = create_matrix(dimention,dimention);
+
+    fillUpMatrices(matrix_a,matrix_b); //COMENTARIO PARA NOSOTROS: Lee los siguientes valores que se ingresan por stdin hasta llegar al \n y corta. Si en algun momento hay algo fuera de lugar
+                                       //ejecuta a "error()"
+
+    matrix_t* matrix_c = matrix_multiply(matrix_a,matrix_b);
+
+    //print_matrix(fp,matrix_c);
+
+    destroy_matrix(matrix_a);
+    destroy_matrix(matrix_b);
+    destroy_matrix(matrix_c);
+
+    /*COMENTARIO PARA NOSOTROS:
+    Resumiendo, hay que hacer las funciones:
+
+    readMatrixDimention
+    create_matrix
+    fillUpMatrices
+    matrix_multiply
+    destroy_matrix
+    error
+    */
+
+    return 0;
+}
---
+++ Tp0.h
@@ -0,0 +1,24 @@
+#ifndef TP0_H_INCLUDED
+#define TP0_H_INCLUDED
+
+
+struct matrix;
+typedef struct matrix matrix_t;
+
+//PRE: RECIBE UN CHAR*
+//POST: DEVUELVE LA DIMENSION O 0(CERO) SI HAY ERROR. NO SE ACEPTA DIMENSION 0.
+int readMatrixDimention(char* dimLeida);
+
+void outputFile(char fileName[]);
+
+//Crea una matriz_t de domension rowsXcols
+matrix_t* create_matrix(size_t rows, size_t cols);
+
+// Destructor de matrix_t
+void destroy_matrix(matrix_t* m);
+
+//Lee los valores que se ingresan por stdin hasta llegar al \n y corta. De haber error corta la ejecucion.
+fillUpMatrices(matrix_a,matrix_b);
+
+
+#endif // TP0_H_INCLUDED
