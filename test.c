15473a165dabaec12e38bf36bcd9acff61868c5f
Axel <axelmpm@gmail.com>
2019-10-01 06:26:18 -0300

    todas las correciones completadas

---
+++ test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+int main(int argc, const char* argv[]){
+
+int a;
+
+a = EOF;
+printf("sample\n");
+printf("%d\n",a);
+printf("sample2\n");
+
+return 0;
+}
