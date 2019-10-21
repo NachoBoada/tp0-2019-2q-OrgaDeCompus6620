ec8f733addca5414b2236add4fc735053c0f66f8
Axel <axelmpm@gmail.com>
2019-10-21 01:17:59 -0300

    se testaron los cambios en el C

--- TP1/tp1.c
+++ TP1/tp1.c
@@ -178,6 +178,7 @@ double* readInput(int* dimention){
         else{
             raiseError("archivo vacio");
             return errorReturnValue;
+        }
     }
     //CHECK IF INPUT IS NUMERIC
     if (returnValue != 1){
@@ -269,7 +270,33 @@ void fillUpMatrices(matrix_t* matrix_a, matrix_t* matrix_b, int dimention,double
     }
 }
 
-matrix_t* matrix_multiply(matrix_t* matrix_a,matrix_t* matrix_b);
+matrix_t* matrix_multiply(matrix_t* matrix_a,matrix_t* matrix_b){
+    int aux1;
+    int aux2;
+    int row;
+    int column;
+    int i;
+    int j;
+    int dimention;
+    double element;
+    dimention = matrix_a->rows;
+    matrix_t* matrix_c;
+    matrix_c = create_matrix(dimention,dimention);
+    for (i = 0; i < dimention*dimention; i++){
+        row = (int)(i / dimention);
+        column = (int)(i % dimention);
+        element = 0;
+        for (j = 0; j < dimention; j++){
+            aux1 = row*dimention;
+            aux1 = aux1 + j;
+            aux1 = matrix_a->array[aux1];
+            aux2 = j*dimention;
+            aux2 = aux2 + column;
+            aux2 = matrix_b->array[aux2];
+            aux1 = aux1 * aux2;
+            element = element + aux1;}
+        matrix_c->array[i] = element;}
+    return matrix_c;}
 
 int print_matrix(FILE* out, matrix_t* matrix_m){
     int dimention = matrix_m->rows;
@@ -337,7 +364,7 @@ int main(int argc, const char* argv[]){
         int dimention;
         input = readInput(&dimention);
 
-        if (input == NULL)){
+        if (input == NULL){
             return 1;
         }
 
@@ -346,6 +373,7 @@ int main(int argc, const char* argv[]){
             free(input);
             raiseError("No se pudo allocar memoria para elementos de matriz");
         }
+
         matrix_b = create_matrix(dimention,dimention);
         if (matrix_b == NULL){
             destroy_matrix(matrix_a);
