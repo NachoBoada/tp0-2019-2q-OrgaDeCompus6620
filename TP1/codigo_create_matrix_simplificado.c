6f384ce15690409f7c363358bef6baa2aa37bda4
Nacho <ignacio.boada@outlook.com>
2019-10-21 00:09:12 -0300

    agregue mymalloc a la carpeta del TP1

--- TP1/codigo_create_matrix_simplificado.c
+++ TP1/codigo_create_matrix_simplificado.c
@@ -1,4 +1,4 @@
-//SIMPLIFICARLO
+//SIMPLIFICARLO (si se puede)
 matrix_t* create_matrix(size_t rows, size_t cols){ // If returns null, there has been an error and it will be manage by the main program.
     matrix_t *matriz = malloc(sizeof(matrix_t));
     if (matriz == NULL){ //si no puede reservar la memoria, deja el puntero en NULL
